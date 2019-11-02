a40772353ea810494e8735ed9436d798d94cef32
jsuriadinata <jsuriadinata@utexas.edu>
2019-11-02 16:26:54 -0500

    Added FlightEngine

---
+++ Flight-Engine/src/Types/index.d.ts
@@ -0,0 +1,48 @@
+declare module 'haversine-distance';
+
+declare interface Aircraft {
+  model: string;
+  speed: number;
+  passengerCapacity: {
+    total: number;
+    main: number;
+    first: number;
+  };
+}
+
+declare interface Airport {
+  code: string; // Airport code, typically 3 characters
+  city: string; // Airport city name
+  location: Location;
+}
+
+declare interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+declare interface FlightDuration {
+  hours: number;
+  minutes: number;
+  locale: string;
+}
+
+declare interface Person{
+  age: description;
+}
+
+declare interface Flight {
+  flightNumber: string;
+  aircraft: Aircraft;
+  origin: Airport;
+  destination: Airport;
+  distance: number;
+  duration: FlightDuration;
+  people_on_flight: Person[];
+}
+
+declare interface FlightQueryParams {
+  date: string;
+  origin?: string;
+  destination?: string;
+}
