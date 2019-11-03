15ded6972e6769b8813d1cb9938b38e306c41d93
Matthew Onghai <Matthew@Matthews-MacBook-Pro-4.local>
2019-11-03 00:43:27 -0500

    Add matching people description

--- frontend.js
+++ frontend.js
@@ -66,7 +66,56 @@ $(document).ready(function(e) {
         //   family: false,
         //   seat_location: 2,
         //   interests: [true, false, false, false, false],
-        console.log("first name: " + result[0].name);
+        console.log(get_flight_num(origin, dest));
+        var seatLocations = ["Aisle", "Center", "Window"];
+        var interestOptions = ["Sports", "Video Games", "Music", "Art", "Travel"];
+        var individInterests = [0, 0, 0, 0, 0];
+        for(var person = 0; person < 5; person++) {
+            for(var i = 0; i < 5; i++) {
+                if(result[0].interests[i]) {
+                    individInterests[person] = interestOptions[i];
+                }
+            }          
+        }
+
+        
+        $("#resultString").text("Results for flight " + get_flight_num(origin, dest));
+        $("#name1").text(result[0].name);
+        $("#name2").text(result[1].name);
+        $("#name3").text(result[2].name);
+        $("#name4").text(result[3].name);
+        $("#name5").text(result[4].name);
+        $("#match-desc1").html("Age: " + result[0].age + "<br/>" +
+                               "First Class: " + result[0].firstClass + "<br/>" +
+                               "Prefers silence: " + result[0].doNotDisturb + "<br/>" +
+                               "Seated with family: " + result[0].family + "<br/>" +
+                               "Seat Location: " + seatLocations[result[0].seat_location] + "<br/>" +
+                               "Interested in: " + individInterests[0] + "<br/>");
+        $("#match-desc2").html("Age: " + result[1].age + "<br/>" +
+                               "First Class: " + result[1].firstClass + "<br/>" +
+                               "Prefers silence: " + result[1].doNotDisturb + "<br/>" +
+                               "Seated with family: " + result[1].family + "<br/>" +
+                               "Seat Location: " + seatLocations[result[1].seat_location] + "<br/>" +
+                               "Interested in: " + individInterests[1] + "<br/>");
+        $("#match-desc3").html("Age: " + result[2].age + "<br/>" +
+                               "First Class: " + result[2].firstClass + "<br/>" +
+                               "Prefers silence: " + result[2].doNotDisturb + "<br/>" +
+                               "Seated with family: " + result[2].family + "<br/>" +
+                               "Seat Location: " + seatLocations[result[2].seat_location] + "<br/>" +
+                               "Interested in: " + individInterests[2] + "<br/>");
+        $("#match-desc4").html("Age: " + result[3].age + "<br/>" +
+                               "First Class: " + result[3].firstClass + "<br/>" +
+                               "Prefers silence: " + result[3].doNotDisturb + "<br/>" +
+                               "Seated with family: " + result[3].family + "<br/>" +
+                               "Seat Location: " + seatLocations[result[3].seat_location] + "<br/>" +
+                               "Interested in: " + individInterests[3] + "<br/>");
+        $("#match-desc5").html("Age: " + result[4].age + "<br/>" +
+                               "First Class: " + result[4].firstClass + "<br/>" +
+                               "Prefers silence: " + result[4].doNotDisturb + "<br/>" +
+                               "Seated with family: " + result[4].family + "<br/>" +
+                               "Seat Location: " + seatLocations[result[4].seat_location] + "<br/>" +
+                               "Interested in: " + individInterests[4] + "<br/>");
+        $("#wrappingDiv").attr("style", "display:block");
     });
 });
 
